c35b7d39a6a325e3961f82317e8b8ce9f3170e1c
Ashwini Rathod <rashwini510@gmail.com>
2022-06-21 00:29:57 +0530

    Added: Add element in between and print the list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,13 +3,14 @@ package com.bridgelabz;
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked List program");
-        LinkedList list = new LinkedList();
-        System.out.println("Adding elements in linked list are:");
-        list.addFirst(56);
-        list.addLast(30);
-        list.addLast(70);
-        list.display();
+        LinkedList linkedList = new LinkedList();
+        System.out.println("Linked list is :");
+        linkedList.addFirstNode(56);
+        linkedList.addLast(70);
+        linkedList.toPrint();
+        System.out.println("List after adding element in middle of the list is");
+        linkedList.addInBetween(56, 70, 30);
+        linkedList.toPrint();
 
     }
 }
--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,11 +1,31 @@
 package com.bridgelabz;
 
 public class Node {
-    public int data;
-    public Node next;
+    int data;
+    Node next;
 
-    public Node(Object data) {
-        this.data = (int) data;
-        this.next = null;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void toPrint() {
+        System.out.println(" " + data);
     }
 }
